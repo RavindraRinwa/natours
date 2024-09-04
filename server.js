55df4ff741c4a9e3d062cbfa8faec2cabe895028
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-09-05 00:26:43 +0530

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -35,7 +40,7 @@ const server = app.listen(port, () => {
 
 //To handle any promise rejection
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
+  console.log(err);
   server.close(() => {
     process.exit(1);
   });
