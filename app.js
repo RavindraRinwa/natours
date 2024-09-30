9e09c060dfd83e1b4001f62c73a82e30e9860dd9
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-01 00:32:30 +0530

    Data sanitization against NoSQL query injection and XSS  using npm packages

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -30,6 +33,10 @@ app.use('/api', limiter);
 //Body parser,reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//Data sanitization against XSS
+app.use(xss());
 //Serving statics files
 app.use(express.static(`${__dirname}/public`));
 
