55c49e10a3ead71fe5b5bd0d96551b3586054c8d
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-08 16:27:00 +0530

    Adding missing Authorization and authenticaion

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,27 +3,23 @@ const express = require('express');
 const authController = require('./../controllers/authController');
 const router = express.Router();
 const userController = require(`${__dirname}/../controllers/userController`);
+
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
+router.patch('/updateMyPassword', authController.updatePassword);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
+router.get('/me', userController.getMe, userController.getUser);
 
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMe', userController.updateMe);
+
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
