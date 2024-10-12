bb6e10eecccb459e1ec532bcf351c2b75c9367f5
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-12 08:45:52 +0530

    feat(review): Prevent multiple reviews per user and implement rating average rounding
    Added unique compound index on tour and user fields in the Review schema to prevent a user from creating more than one review per tour.
    Implemented a set function for the rating field to automatically round the rating average to one decimal place.
    Enhanced data integrity by ensuring that users cannot submit duplicate reviews for the same tour.

--- models/tourModel.js
+++ models/tourModel.js
@@ -35,6 +35,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be less or equal to 5.0'],
+      set: (val) => Math.round(val * 10) / 10, //4.666 -> 5 46.66->47 then divide by 10
     },
     ratingQuantity: {
       type: Number,
