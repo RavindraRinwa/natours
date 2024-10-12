f29d3a11f62695bf9862b05f720b09e4722f296c
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-12 08:23:18 +0530

    feat(review): Implement average rating calculation for tours
    Added a static method calcAverageRatings in the Review model to compute the average rating and rating quantity for a specific tour.
    Updated the tour document's ratingQuantity and ratingAverage fields based on the aggregation of reviews.
    Implemented post('save') middleware to trigger the recalculation of ratings each time a review is added.
    Added pre and post middleware for findOneAndUpdate and findOneAndDelete to ensure that tour ratings are updated when reviews are modified or deleted.
    Improved query execution by ensuring that the review document is accessed only once during the middleware lifecycle.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -47,5 +48,55 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+//here this point to Model
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  if (stats[0].length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: stats[0].nRating,
+      ratingAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: 0,
+      ratingAverage: 4.5,
+    });
+  }
+};
+
+// this.constructer point to current MODEL
+reviewSchema.post('save', function () {
+  //this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+//findByIdAndUpdate
+//findByIdAndDelete
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.model.findOne(this.getQuery()); // to access the review model in query middle ware
+  // console.log(this.r);
+  //save in current query so that i can access in post
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // this.r = await this.findOne(); does NOT work here,query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
+
 module.exports = Review;
