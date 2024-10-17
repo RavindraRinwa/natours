2d2cbdacc4831d10bfaa31de94fb8fc13cc58d39
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-17 21:28:48 +0530

    Revert "Add feature to calculate distance from all tour starting points and integrate Pug templates"
    This reverts commit 11514da966b81143ef7ff427e8f4e4e0ab565369.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -53,8 +53,6 @@ router
 //  /tours-within?distance=233&center=-40,45&unit=mi
 //   /tours-within/233/center/40,45/unit/mi
 
-router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
-
 router
   .route('/monthly-plan/:year')
   .get(
