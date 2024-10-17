6c7666913d3605642ed602f0d4ab5ac5395e711b
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-18 00:48:08 +0530

    Implement overview page with tour details and dynamic content rendering

---
+++ routes/viewRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const viewsController = require('../controllers/viewController');
+
+//ROUTES
+router.get('/', viewsController.getOverivew);
+router.get('/tour', viewsController.getTour);
+
+module.exports = router;
