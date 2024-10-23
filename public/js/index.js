3f3f6a7d64f9075371790c65b24f70b5ccd7fcab
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-23 23:58:23 +0530

    Logging Out Users

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,19 @@
 import '@babel/polyfill';
-import { login } from './login';
+import { login, logout } from './login';
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+// DOM ELEMENTS
+const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
+
+if (logOutBtn) {
+  logOutBtn.addEventListener('click', logout);
+}
